ffd55fc588e05bc52a80f96d8c3155e1c03396d9
Mark Lockett <mark@deliveryengine.net>
2020-09-06 13:09:56 +0800

    Add unit test for LiteDB that refers to BsonExpressions

---
+++ litedbasynctest/Database/Snapshot_Upgrade_Tests.cs
@@ -0,0 +1,28 @@
+﻿using LiteDB;
+using LiteDB.Async;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.LiteDB.Async
+{
+    public class Snapshot_Upgrade_Tests
+    {
+        [Fact]
+        public async Task Transaction_Update_Upsert()
+        {
+            using var db = new LiteDatabaseAsync(":memory:");
+            var col = db.GetCollection("test");
+
+            // Transactions not supported ATM by Async
+            //bool transactionCreated = db.BeginTrans();
+            //Assert.True(transactionCreated);
+
+            int updatedDocs = await  col.UpdateManyAsync("{name: \"xxx\"}", BsonExpression.Create("_id = 1"));
+            Assert.Equal(0, updatedDocs);
+
+            await col.UpsertAsync(new BsonDocument() { ["_id"] = 1, ["name"] = "xxx" });
+            var result = await col.FindByIdAsync(1);
+            Assert.NotNull(result);
+        }
+    }
+}
