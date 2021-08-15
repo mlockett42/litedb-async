9364789d8022765933141eb970ce9ddf935156db
Mark Lockett <mark@squarecode.com.au>
2021-08-15 19:29:07 +0800

    Clean up code

--- litedbasynctest/engine/Transaction_Tests.cs
+++ litedbasynctest/engine/Transaction_Tests.cs
@@ -1,8 +1,6 @@
 using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using FluentAssertions;
 using LiteDB.Engine;
 using Xunit;
 using LiteDB.Async;
@@ -13,9 +11,8 @@ namespace Tests.LiteDB.Async
     public class Transactions_Tests
     {
         [Fact]
-        public async Task Transactions_Not_Allowed_On_Memmory_Streams()
+        public async Task Transactions_Allowed_On_Memmory_Streams()
         {
-            //using (var db = new LiteDatabase("filename=:memory:"))
             using (var asyncDb = new LiteDatabaseAsync(new MemoryStream()))
             {
                 var transDb = await asyncDb.BeginTransactionAsync();
@@ -23,7 +20,7 @@ namespace Tests.LiteDB.Async
         }
 
         [Fact]
-        public async Task Transactions_Not_Allowed_On_Direct_Mode_Files()
+        public async Task Transactions__Allowed_On_Direct_Mode_Files()
         {
             var connectionString = new ConnectionString()
             {
@@ -32,7 +29,6 @@ namespace Tests.LiteDB.Async
                 Password = "hunter2"
             };
 
-            //using (var db = new LiteDatabase(connectionString))
             using (var asyncDb = new LiteDatabaseAsync(connectionString))
             {
                 var transDb = await asyncDb.BeginTransactionAsync();
@@ -49,7 +45,6 @@ namespace Tests.LiteDB.Async
                 Password = "hunter2"
             };
 
-            //using (var db = new LiteDatabase(connectionString))
             using (var asyncDb = new LiteDatabaseAsync(connectionString))
             {
                 // Verify this function just does not throw an exception
@@ -94,61 +89,6 @@ namespace Tests.LiteDB.Async
 
             });
             Assert.StartsWith("LiteDb encounter an error.", exception.Message);
-            //TODO: Change this test to use the new transaction infrastructure
-            /*
-            var data1 = DataGen.Person(1, 100).ToArray();
-            var data2 = DataGen.Person(101, 200).ToArray();
-
-            using (var db = new LiteDatabase("filename=:memory:"))
-            using (var asyncDb = new LiteDatabaseAsync(db, false))
-            {
-                // small timeout
-                await asyncDb.PragmaAsync(Pragmas.TIMEOUT, 1);
-
-                var asyncPerson = asyncDb.GetCollection<Person>();
-                var person = db.GetCollection<Person>();
-
-                // init person collection with 100 document
-                await asyncPerson.InsertAsync(data1);
-
-                var taskASemaphore = new SemaphoreSlim(0, 1);
-                var taskBSemaphore = new SemaphoreSlim(0, 1);
-
-                // task A will open transaction and will insert +100 documents 
-                // but will commit only 2s later
-                var ta = Task.Run(async () =>
-                {
-                    await asyncDb.BeginTransAsync();
-
-                    await asyncPerson.InsertAsync(data2);
-
-                    taskBSemaphore.Release();
-                    taskASemaphore.Wait();
-
-                    var count = await asyncPerson.CountAsync();
-
-                    count.Should().Be(data1.Length + data2.Length);
-
-                    await asyncDb.CommitAsync();
-                });
-
-                // task B will try delete all documents but will be locked during 1 second
-                var tb = Task.Run(() =>
-                {
-                    taskBSemaphore.Wait();
-
-                    db.BeginTrans();
-                    person
-                        .Invoking(personCol => personCol.DeleteMany("1 = 1"))
-                        .Should()
-                        .Throw<LiteException>()
-                        .Where(ex => ex.ErrorCode == LiteException.LOCK_TIMEOUT);
-
-                    taskASemaphore.Release();
-                });
-
-                await Task.WhenAll(ta, tb);
-            }*/
         }
 
         [Fact]
@@ -311,63 +251,6 @@ public async Task Transaction_Read_Version()
 
             // Attempt to read from the second connection again should be 100 records. We shouldn't see the second lot of records because we are already in a transaction
             Assert.Equal(100, await asyncPerson3.CountAsync());
-
-            /*
-var data1 = DataGen.Person(1, 100).ToArray();
-var data2 = DataGen.Person(101, 200).ToArray();
-
-using (var db = new LiteDatabase(new MemoryStream()))
-using (var asyncDb = new LiteDatabaseAsync(db, false))
-{
-var asyncPerson = asyncDb.GetCollection<Person>();
-var person = db.GetCollection<Person>();
-
-// init person collection with 100 document
-await asyncPerson.InsertAsync(data1);
-
-var taskASemaphore = new SemaphoreSlim(0, 1);
-var taskBSemaphore = new SemaphoreSlim(0, 1);
-
-// task A will insert more 100 documents but will commit only 1s later
-var ta = Task.Run(async () =>
-{
-await asyncDb.BeginTransAsync();
-
-await asyncPerson.InsertAsync(data2);
-
-taskBSemaphore.Release();
-taskASemaphore.Wait();
-
-await asyncDb.CommitAsync();
-
-taskBSemaphore.Release();
-});
-
-// task B will open transaction too and will count 100 original documents only
-// but now, will wait task A finish - but is in transaction and must see only initial version
-var tb = Task.Run(() =>
-{
-db.BeginTrans();
-
-taskBSemaphore.Wait();
-
-var count = person.Count();
-
-// read 100 documents
-count.Should().Be(data1.Length);
-
-taskASemaphore.Release();
-taskBSemaphore.Wait();
-
-// keep reading 100 documents because i'm still in same transaction
-count = person.Count();
-
-count.Should().Be(data1.Length);
-});
-
-await Task.WhenAll(ta, tb);
-}
-            */
         }
 
         [Fact]
@@ -424,41 +307,6 @@ await Task.WhenAll(ta, tb);
                 await asyncPerson2.InsertAsync(data1);
             });
             Assert.Equal("Transaction Closed, no further writes are allowed.", exception.Message);
-            /*
-var data0 = DataGen.Person(1, 10).ToArray();
-var data1 = DataGen.Person(11, 20).ToArray();
-
-using (var db = new LiteDatabaseAsync(new MemoryStream()))
-{
-var person = db.GetCollection<Person>();
-
-// first time transaction will be opened
-(await db.BeginTransAsync()).Should().BeTrue();
-
-// but in second type transaction will be same
-(await db.BeginTransAsync()).Should().BeFalse();
-
-await person.InsertAsync(data0);
-
-// must commit transaction
-(await db.CommitAsync()).Should().BeTrue();
-
-// no transaction to commit
-(await db.CommitAsync()).Should().BeFalse();
-
-// no transaction to rollback;
-(await db.RollbackAsync()).Should().BeFalse();
-
-(await db.BeginTransAsync()).Should().BeTrue();
-
-// no page was changed but ok, let's rollback anyway
-(await db.RollbackAsync()).Should().BeTrue();
-
-// auto-commit
-await person.InsertAsync(data1);
-
-(await person.CountAsync()).Should().Be(20);
-}*/
         }
     }
             
