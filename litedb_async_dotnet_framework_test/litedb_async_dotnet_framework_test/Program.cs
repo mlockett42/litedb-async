35fe4b13777926d195a2ae6ed21e2f4456b5483f
Mark Lockett <mark@squarecode.com.au>
2023-01-16 21:19:19 +0800

    Use a strong name for the console app, this causes it to error because the required litedb.async v0.1.3 is not signed for strong names

---
+++ litedb_async_dotnet_framework_test/litedb_async_dotnet_framework_test/Program.cs
@@ -0,0 +1,21 @@
+﻿using LiteDB.Async;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace litedb_async_dotnet_framework_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new LiteDatabaseAsync(new MemoryStream());
+
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
+    }
+}
